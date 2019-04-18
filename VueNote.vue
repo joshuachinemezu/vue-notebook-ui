6cb90002ba8b6a8fadb2c3630bdb50e1fc2a3ede
Joshua Chinemezu <joshuachinemezu@gmail.com>
2019-04-18 12:34:16 +0100

    Add EmptyLined, EmptyLinedColored and Texture paper types

---
+++ VueNote.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <notebook-ui content="Amazing" placeholder="This is it" type="Texture" color="red">Wow</notebook-ui>
+  </div>
+</template>
+
+<script>
+import NotebookUi from "./src/NotebookUi";
+export default {
+  components: {
+    NotebookUi
+  }
+};
+</script>
---
+++ src/NotebookUi.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="[
+   'note-area--' + type,
+   'note-area-margin--' + type,
+  ]">
+    <textarea
+      :class="[
+    'note-area-content--' + type,
+    ]"
+      :placeholder="placeholder"
+      :value="content"
+      :style="{ color: color, }"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+import "./css/EmptyLined.css";
+import "./css/EmptyLinedColored.css";
+import "./css/Texture.css";
+
+export default {
+  name: "notebook-ui",
+  props: {
+    type: {
+      type: String,
+      default: "EmptyLined",
+      validator(x) {
+        return ["EmptyLined", "EmptyLinedColored", "Texture"].indexOf(x) !== -1;
+      }
+    },
+    color: {
+      type: String,
+      default: "mediumblue"
+    },
+    placeholder: { default: "Write note here" },
+    content: { default: "Wow" }
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
